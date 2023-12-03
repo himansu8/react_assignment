49b8372e88f1861a886011e052e792409aa15dda
himansu8 <himansusekharsahu50@gmail.com>
2023-12-03 14:05:43 +0530

    jokes api

---
+++ assignment_6/src/Components/Home.jsx
@@ -0,0 +1,13 @@
+// function Home (props){
+//     //console.log(props)
+//     return(
+//         <>
+//         {props.jokes.map(ele =>{
+//             return(
+//                 <p>jokes : </p>
+//             )
+//         })}
+//         </>
+//     )
+// }
+// export default Home;
