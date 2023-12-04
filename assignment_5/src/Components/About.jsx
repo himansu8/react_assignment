9b357d7cf4e02ca2e751d05b024d39434e1183c8
himansu8 <himansusekharsahu50@gmail.com>
2023-12-04 23:29:31 +0530

    navigation link

--- assignment_5/src/Components/About.jsx
+++ assignment_5/src/Components/About.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 function About(){
     return (
         <>
         <h1>about THS</h1>
+        <Link to="/">home</Link>
+        
         </>
     )
 }
--- assignment_5/src/Components/Home.jsx
+++ assignment_5/src/Components/Home.jsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 function Home(){
     return (
         <>
                 <h1>THS home</h1>
-
+                <Link to="/about">about</Link>
+                <br/>
+                <a href="/about">about a tag</a>
         </>
     )
 }
